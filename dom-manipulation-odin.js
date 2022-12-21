b20abebb8f7434ae6eb025c34345164d7b363bfb
Sridhar Bhagavathula <sridhar.bhagavathula@gmail.com>
2022-12-21 08:25:44 -0500

    Create event listener buttons and functions

--- dom-manipulation-odin.js
+++ dom-manipulation-odin.js
@@ -24,3 +24,14 @@ fourthLine.textContent = "ME TOO!";
 coloredBox.appendChild(fourthLine);
 
 container.appendChild(coloredBox);
+
+
+function alertFunction() {
+    alert("YAY! YOU DID IT!");
+}
+
+const btn2 = document.querySelector('#btn2');
+btn2.onclick = alertFunction;
+
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', alertFunction);
