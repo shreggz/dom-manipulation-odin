eb7d89013cb3727cc813de5440c91a1db3f4c066
Sridhar Bhagavathula <sridhar.bhagavathula@gmail.com>
2022-12-21 07:41:24 -0500

    Create child div with colored box

--- dom-manipulation-odin.js
+++ dom-manipulation-odin.js
@@ -1,7 +1,26 @@
-const container = document.querySelector('#container');
+const firstLine = document.createElement('p');
+firstLine.classList.add('firstLine');
+firstLine.textContent = "Hey I'm red!";
+firstLine.style.color = 'red';
+container.appendChild(firstLine);
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
 
-container.appendChild(content);
+const secondLine = document.createElement('h3');
+secondLine.classList.add('secondLine');
+secondLine.textContent = "I'm a blue h3!";
+secondLine.style.color = 'blue';
+container.appendChild(secondLine);
+
+const coloredBox = document.createElement('div');
+coloredBox.classList.add('coloredBox');
+coloredBox.style.cssText = 'background-color: pink; border-color: black; border-style: solid;';
+
+const thirdLine = document.createElement('h1');
+thirdLine.textContent = "I'm in a div";
+coloredBox.appendChild(thirdLine);
+
+const fourthLine = document.createElement('p');
+fourthLine.textContent = "ME TOO!";
+coloredBox.appendChild(fourthLine);
+
+container.appendChild(coloredBox);
